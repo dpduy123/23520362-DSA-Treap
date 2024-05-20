f2c4b078aba332cd28fade9cc8d9d4d26ab6987a
dpduy123 <161210550+dpduy123@users.noreply.github.com>
2024-05-20 19:39:09 +0700

    Add files via upload

---
+++ ReversalsandSums.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+#define int long long 
+#define pii pair <int,int> 
+#define st first 
+#define nd second 
+#define vi vector <int> 
+#define vpii vector <pii> 
+#define pb push_back 
+#define lb lower_bound 
+#define ub upper_bound 
+#define FILE ""
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r) { return uniform_int_distribution<int>(l, r)(rd); }
+
+const int oo = 1e18;
+const int base = 311;
+const int mod = 998244353;
+
+const int N = 1e6 + 6;
+const int MAX = 2e9;
+
+int getpri() {return rand(0, MAX);}
+struct Node {
+	int val, weight, size;
+	Node *left, *right;
+	bool inv;
+	int sum;
+	Node() {}
+	Node(int val) : val(val), size(1), weight(getpri()), inv(0), sum(val), left(NULL), right(NULL){};
+} *root;
+
+int size(Node *cur) {return cur ? cur->size : 0;}
+int sum(Node *cur) {return cur ? cur->sum : 0;}
+
+void pushdown(Node *cur) 
+{
+	if (!cur) return;
+	if (!cur->inv) return;
+	swap(cur->left, cur->right);
+	if (cur->left) cur->left->inv ^= 1;
+	if (cur->right) cur->right->inv ^= 1;
+	cur->inv = 0;
+}
+void split(Node *treap, Node *&left, Node *&right, int key, int add = 0) {
+	pushdown(treap);
+	if (!treap) {
+		left = right = NULL;
+		return;
+	}
+
+	int cur_size = add + size(treap->left);  // implicit key
+	if (cur_size <= key) {
+		split(treap->right, treap->right, right, key, add + 1 + size(treap->left));
+		left = treap;
+	} else {
+		split(treap->left, left, treap->left, key, add); 
+		right = treap;
+	}
+	treap->size = 1 + size(treap->left) + size(treap->right);
+	treap->sum = sum(treap->left) + sum(treap->right) + treap->val;
+}
+
+void merge(Node *&root, Node *left, Node *right) {
+	pushdown(left);
+	pushdown(right);
+	if (!left || !right) {
+		root = left ? left : right;
+		return;
+	}
+	if (left->weight > right->weight) {
+		merge(left->right, left->right, right);
+		root = left;
+	} else {
+		merge(right->left, left, right->left);
+		root = right;
+	}
+	root->size = 1 + size(root->left) + size(root->right);
+	root->sum = sum(root->left) + sum(root->right) + root->val;
+}
+
+void print(Node *cur) {
+	if (!cur) return;
+	print(cur->left);
+	cout << cur->val;
+	print(cur->right);
+}
+
+int n, m, a[N];
+void input()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) 
+	{
+		cin >> a[i];
+		merge(root, root, new Node(a[i]));
+	}
+}	
+
+void solve()
+{
+	while (m--) 
+	{
+		int type, l, r;
+		cin >> type >> l >> r;
+		Node *a, *b, *c, *d;
+		if (type == 1) 
+		{
+			split(root, a, b, r - 1, 0);
+			split(a, c, d, l - 2, 0);
+			d->inv ^= 1;
+			merge(root, c, d);
+			merge(root, root, b);
+		}
+		if (type == 2) {
+			split(root, a, b, r - 1, 0);
+			split(a, c, d, l - 2, 0);
+			cout << sum(d) << "\n";
+			merge(root, c, d);
+			merge(root, root, b);
+		}
+	}
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int T = 1;
+	//cin >> T;
+	while (T--) 
+	{
+		input();
+		solve();
+	}
+	return 0;
+}
+/*
+From Benq: 
+	int overflow?
+	array bounds?
+	Special Cases (n = 1?)
+	Do something instead of nothing and stay organized 
+	WRITE STUFF DOWN 
+	DON'T GET STUCK ON ONE APPROACH 
+*/
