e0c0e3fd29a4b2f0d3e7f4d2280894fbec2ab56b
dpduy123 <161210550+dpduy123@users.noreply.github.com>
2024-05-20 18:17:02 +0700

    Update Initial.cpp

--- Initial.cpp
+++ Initial.cpp
@@ -1,7 +1,9 @@
 int getpri() {return rand(0, MAX);}
 struct Node {
-	// the value and weightority of the node respectively
-	int val, weight, size;
+	// the value and priority of the node respectively
+	int val, weight;
+	// Biến size là số lượng nút của cây
+	int size;
 	// pointer to left and right child (NULL means no child)
 	Node *left, *right;
 	Node(int val) : val(val), size(1), weight(getpri()), left(NULL), right(NULL){};
